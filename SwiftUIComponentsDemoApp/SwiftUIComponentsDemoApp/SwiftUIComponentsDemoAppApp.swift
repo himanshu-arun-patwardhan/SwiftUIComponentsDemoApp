09e5fc6b4b650ceca5b3d9a880b394736349626a
Himanshu Arun Patwardhan <himanshu.arun.patwardhan@gmail.com>
2026-02-11 12:38:02 +0530

    Xcode new project - App - SwiftUI - basic initial setup

---
+++ SwiftUIComponentsDemoApp/SwiftUIComponentsDemoApp/SwiftUIComponentsDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIComponentsDemoAppApp.swift
+//  SwiftUIComponentsDemoApp
+//
+//
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIComponentsDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
